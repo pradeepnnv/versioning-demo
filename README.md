3cad46ded4dfc1c04bb62115c9add804ff214a25
Pradeep Namburi <pnamburi@cccis.com>
2019-10-22 18:40:48 -0500

    Version Demo

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+Reference [https://github.com/kalaspuffar/VersioningDemo]
+
 # versioning-demo
 Versioning Demo - Maven Release
+
+### Build the maven package
+```
+mvn package
+```
+
+### Test
+```
+java -jar target/mvn-version-demo-1.0-SNAPSHOT-jar-with-dependencies.jar
+```
---
+++ src/main/java/org/pradeepnnv/versiondemo/VersionDemo.java
@@ -0,0 +1,9 @@
+package org.pradeepnnv.versiondemo;
+
+public class VersionDemo {
+  public static void main(String[] args) {
+    System.out.println("Hey there!!!"+"\n");
+    System.out.println("This program is running version " +
+               VersionDemo.class.getPackage().getImplementationVersion());
+  }
+}
