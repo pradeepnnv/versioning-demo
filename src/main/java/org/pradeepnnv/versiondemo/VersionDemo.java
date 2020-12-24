2aedd104ccf9fa45840b7f80f81dc1d5b42d7ede
pradeepnnv <pradeepnnv@gmail.com>
2020-12-24 11:13:18 -0600

    Remove the test line
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- src/main/java/org/pradeepnnv/versiondemo/VersionDemo.java
+++ src/main/java/org/pradeepnnv/versiondemo/VersionDemo.java
@@ -3,7 +3,6 @@ package org.pradeepnnv.versiondemo;
 public class VersionDemo {
   public static void main(String[] args) {
     System.out.println("Hey there!!!"+"\n");
-    System.out.println("This is a test!!");
     System.out.println("This program is running version " +
                VersionDemo.class.getPackage().getImplementationVersion());
   }
