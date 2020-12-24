8a5396878adb92c5ac28f28b9a49c9721d3f9ceb
pradeepnnv <pradeepnnv@gmail.com>
2020-12-24 11:56:32 -0600

    Make an update
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- src/main/java/org/pradeepnnv/versiondemo/VersionDemo.java
+++ src/main/java/org/pradeepnnv/versiondemo/VersionDemo.java
@@ -2,7 +2,7 @@ package org.pradeepnnv.versiondemo;
 
 public class VersionDemo {
   public static void main(String[] args) {
-    System.out.println("Hey there!!!"+"\n");
+    System.out.println("Hey there!!!!"+"\n");
     System.out.println("This program is running version " +
                VersionDemo.class.getPackage().getImplementationVersion());
   }
