d3bf7819a5c0d38b79cc0e34fe81c7889e1dce2a
pradeepnnv <pradeepnnv@gmail.com>
2020-12-24 11:37:40 -0600

    Adding for test
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- src/main/java/org/pradeepnnv/versiondemo/VersionDemo.java
+++ src/main/java/org/pradeepnnv/versiondemo/VersionDemo.java
@@ -3,6 +3,7 @@ package org.pradeepnnv.versiondemo;
 public class VersionDemo {
   public static void main(String[] args) {
     System.out.println("Hey there!!!"+"\n");
+    System.out.println("Test!!");
     System.out.println("This program is running version " +
                VersionDemo.class.getPackage().getImplementationVersion());
   }
